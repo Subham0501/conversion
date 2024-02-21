fe102bdc8f3f5d98751e2ecd81ebaa23819bf0fe
subhamRp <137407728+subhamRp@users.noreply.github.com>
2024-02-21 14:36:51 +0545

    Initial commit

---
+++ playground.py
@@ -0,0 +1,11 @@
+def add(*args):
+    print(args[0])
+    sum=0
+    for n in args:
+        sum+=n
+    return sum
+print(add(3,5,6))
+def calculate(**kwargs):
+    print(kwargs)
+calculate(add=3,multiply=5)
+calculate()
